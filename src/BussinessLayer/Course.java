8e638ef31a301ad7ed340066efeaa41fdb50aa11
msahmoudr <msahmoudr@gmail.com>
2025-11-15 22:05:55 +0200

    Update Course.java

--- src/BussinessLayer/Course.java
+++ src/BussinessLayer/Course.java
@@ -16,7 +16,42 @@ public class Course {
     private String description;
     private String instructorId;
     private ArrayList<Lesson> lessons;
-    private String [][] enrolledStudents;
+    private ArrayList<String> enrolledStudents;
+    public Course(String courseId, String title, String description, String instructorId, ArrayList<Lesson> lessons, ArrayList<String> enrolledStudents) {
+        this.courseId = courseId;
+        this.title = title;
+        this.description = description;
+        this.instructorId = instructorId;
+        this.lessons = lessons;
+        this.enrolledStudents = enrolledStudents;
+    }
+    
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public String getInstructorId() {
+        return instructorId;
+    }
+
+    public ArrayList<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public ArrayList<String> getEnrolledStudents() {
+        return enrolledStudents;
+    }
+
+
     
     
     
