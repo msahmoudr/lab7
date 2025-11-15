f25217c341cbf43268edbccffd2ea77ee36232d6
abdullah10-30 <abdullahelsayyed1807@gmail.com>
2025-11-16 01:08:21 +0200

    add attributes and constructor

--- src/BussinessLayer/InstructorController.java
+++ src/BussinessLayer/InstructorController.java
@@ -1,4 +1,28 @@
 package BussinessLayer;
 
-public class InstructorController {
+import DataAccessLayer.JsonFileHandler;
+
+import java.util.ArrayList;
+
+public class InstructorController
+{
+
+    private Instructor CurrentInstructor;
+    public InstructorController(String userId)
+    {
+
+        this.CurrentInstructor = GetInstructorById(userId);
+    }
+
+    private Instructor GetInstructorById(String userId)
+    {
+        ArrayList<Instructor> Instructors = JsonFileHandler.returnInstructors();
+        return  Instructors.stream().filter(s -> s.getUserId().equals(userId)).findFirst().get();
+    }
+
+    public Instructor getCurrentInstructor() {
+        return CurrentInstructor;
+    }
+
+    
 }
