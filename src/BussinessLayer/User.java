c4a6ed11bcb4852ebe4c25d1d03eb5fa68c59c41
abdullah10-30 <abdullahelsayyed1807@gmail.com>
2025-11-15 22:37:43 +0200

    add user attributes

--- src/BussinessLayer/User.java
+++ src/BussinessLayer/User.java
@@ -2,5 +2,17 @@ package BussinessLayer;
 
 public class User
 {
-    
+    private String userId;
+    private String userName;
+    private String email;
+    private String passwordHash;
+    private boolean role;
+
+    public User(String userId, String userName, String email, String passwordHash, boolean role) {
+        this.userId = userId;
+        this.userName = userName;
+        this.email = email;
+        this.passwordHash = passwordHash;
+        this.role = role;
+    }
 }
