0ab4dd418f04504d00b4f0aee182be7f1692ea37
msahmoudr <msahmoudr@gmail.com>
2025-11-15 15:48:45 +0200

    Update Course.java

--- src/BussinessLayer/Course.java
+++ src/BussinessLayer/Course.java
@@ -4,10 +4,21 @@
  */
 package BussinessLayer;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author msahm
  */
 public class Course {
+    private String courseId;
+    private String title;
+    private String description;
+    private String instructorId;
+    private ArrayList<Lesson> lessons;
+    private ArrayList<Student> enrolledStudents;
+    
+    
+    
     
 }
