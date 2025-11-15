dace9e07eab801b21ea2861fd4c808078a362ec7
abdullah10-30 <abdullahelsayyed1807@gmail.com>
2025-11-15 22:57:41 +0200

    create json files

---
+++ src/DataAccessLayer/JsonFiles/FilesName.java
@@ -0,0 +1,14 @@
+package DataAccessLayer.JsonFiles;
+
+import lab7.Lab7;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FilesName
+{
+    public String usersFile = "DataAccessLayer/JsonFiles/users.json";
+    public String coursesFile = "DataAccessLayer/JsonFiles/courses.json";
+
+
+}
