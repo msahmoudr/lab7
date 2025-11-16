61e0e6f5e78982eda510841aa33d50bcd0546df0
msahmoudr <msahmoudr@gmail.com>
2025-11-16 13:43:46 +0200

    Update LessonController.java

--- src/BussinessLayer/LessonController.java
+++ src/BussinessLayer/LessonController.java
@@ -4,10 +4,21 @@
  */
 package BussinessLayer;
 
+import DataAccessLayer.JsonFileHandler;
+
+import java.util.ArrayList;
+
 /**
  *
  * @author msahm
  */
 public class LessonController {
+    CourseController cc =new CourseController();
+    public void addLesson (Course course,Lesson lesson)
+    {
+        CourseController.addLesson(course, lesson);
+
+
+    }
     
 }
